3380a5c603b196dfb088a07730f11ddc120f3502
Dina sourani <dyxz8613@gmail.com>
2025-12-12 13:43:53 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication6
+{
+    class Program
+    {
+        static void arr(int[] arra, int index, Random rnd)
+        {
+       
+            if (index == arra.Length)
+                return;
+
+      
+            arra[index] = rnd.Next(0, 101);
+
+            
+            arr(arra, index + 1, rnd);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("تعداد عناصر آرایه را وارد کنید: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[] array = new int[n];
+            Random rnd = new Random();
+
+            // مقداردهی بازگشتی
+            arr(array, 0, rnd);
+
+            // نمایش مقادیر آرایه
+            Console.WriteLine("\nمقادیر آرایه:");
+            foreach (int x in array)
+            {
+                Console.Write(x + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
